4198ef2b19a20d0a080b394449a26a749a021f95
ViorelsS <viorelss@icloud.com>
2025-03-20 17:41:33 +0100

    ADD: semantic and similarity search

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 import { openai, supabase } from "./config";
-import podcasts from "./content";
+
+const query = "What can I listen to in half of hour?";
 
 const main = async (input) => {
-    const data = await Promise.all(
-        input.map(async (textChunk) => {
-            const embeddingResponse = await openai.embeddings.create({
-                model: "text-embedding-ada-002",
-                input: textChunk,
-            });
+    /* Create a vector embedding representing the input text */
+    const embeddingResponse = await openai.embeddings.create({
+        model: "text-embedding-ada-002",
+        input,
+    });
 
-            return {
-                context: textChunk,
-                embedding: embeddingResponse.data[0].embedding,
-            };
-        })
-    );
+    /* The vector generate by OpenAI */
+    const embedding = embeddingResponse.data[0].embedding;
 
-    /* Insert content ad embedding into Supabase */
-    await supabase.from("documents").insert(data);
+    /* Query supabase for nearest vector match */
+    const { data } = await supabase.rpc("match_documents", {
+        query_embedding: embedding,
+        match_threshold: 0.5,
+        match_count: 3,
+    });
 
-    console.log("Embedding and storing complete!");
+    console.log(data[0].context, data[0].similarity);
 };
 
-main(podcasts);
+main(query);
